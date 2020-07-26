cd74c73847d35da69ba30a96871b5d942113a00f
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2020-07-26 12:00:16 +1000

    Agrega funcionalidad para subir un escudo

--- src/config/di.js
+++ src/config/di.js
@@ -1,6 +1,9 @@
 // configure DI container
+const path = require('path');
 const { default: DIContainer, object, get, factory } = require('rsdi');
 const { Sequelize } = require('sequelize');
+const multer = require('multer');
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const { ClubController, ClubService, ClubRepository, ClubModel } = require('../module/club/module');
@@ -22,8 +25,11 @@ function configureSessionSequelizeDatabase() {
   return sequelize;
 }
 
-function configureClubModule(di) {
-  return ClubModel.setup(di.get('Sequelize'));
+/**
+ * @param {DIContainer} container
+ */
+function configureClubModule(container) {
+  return ClubModel.setup(container.get('Sequelize'));
 }
 
 /**
@@ -45,17 +51,32 @@ function configureSession(container) {
   return session(sessionOptions);
 }
 
-module.exports = function configureDI(app) {
+function configureMulter() {
+  const storage = multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, process.env.CRESTS_UPLOAD_DIR);
+    },
+    filename(req, file, cb) {
+      // https://stackoverflow.com/questions/31592726/how-to-store-a-file-with-file-extension-with-multer
+      // al tener una extensión, el navegador lo sirve en vez de descargarlo
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  });
+
+  return multer({ storage });
+}
+
+module.exports = function configureDI() {
   const container = new DIContainer();
   container.addDefinitions({
-    ClubController: object(ClubController).construct(get('ClubService')),
+    ClubController: object(ClubController).construct(get('Multer'), get('ClubService')),
     ClubService: object(ClubService).construct(get('ClubRepository')),
     ClubRepository: object(ClubRepository).construct(get('ClubModel')),
     ClubModel: factory(configureClubModule),
     Sequelize: factory(configureMainSequelizeDatabase),
     SessionSequelize: factory(configureSessionSequelizeDatabase),
     Session: factory(configureSession),
-    App: app,
+    Multer: factory(configureMulter),
   });
   return container;
 };
